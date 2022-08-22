d0764ad9bdbdf30d8bc832a6d30e379ce08dee24
anthonymccanny <anthony.mccanny@gmail.com>
2022-08-22 19:23:26 -0400

    individual analyses updates

--- individual_analyses.R
+++ individual_analyses.R
@@ -17,5 +17,51 @@ for(countryname in countries) {
 }
 
 tab_model(models, dv.labels = countries, file="individual_country_output.html")
-models[1]
-dv.labels = c("Armenia", "OtherCountry")
+
+
+### POSITIVE AFFECT ###
+i<- 1
+summaries <- list()
+models <- list()
+
+for(countryname in countries) {
+  country <- filter(assas, country==countryname)
+  pa.model <- lm(pa ~ year_number + after + year_after + loggdp_z + conflict_z, country, weight=weight)
+  summaries[[i]] <- summary(pa.model)
+  models[[i]] <- pa.model
+  i <- i+1
+}
+
+tab_model(models, dv.labels = countries, file="individual_country_output_pa.html")
+
+
+### NEGATIVE AFFECT ###
+i<- 1
+summaries <- list()
+models <- list()
+
+for(countryname in countries) {
+  country <- filter(assas, country==countryname)
+  na.model <- lm(na ~ year_number + after + year_after + loggdp_z + conflict_z, country, weight=weight)
+  summaries[[i]] <- summary(na.model)
+  models[[i]] <- na.model
+  i <- i+1
+}
+
+tab_model(models, dv.labels = countries, file="individual_country_output_na.html")
+
+
+### HOPE ###
+i<- 1
+summaries <- list()
+models <- list()
+
+for(countryname in countries) {
+  country <- filter(assas, country==countryname)
+  hope.model <- lm(hope ~ year_number + after + year_after + loggdp_z + conflict_z, country, weight=weight)
+  summaries[[i]] <- summary(hope.model)
+  models[[i]] <- hope.model
+  i <- i+1
+}
+
+tab_model(models, dv.labels = countries, file="individual_country_output_hope.html")
