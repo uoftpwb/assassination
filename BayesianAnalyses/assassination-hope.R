e0cf388772cd2a7154efc6ae6e9009058501eb69
anthonymccanny <anthony.mccanny@gmail.com>
2022-09-28 10:14:41 -0400

    Bayesian Update...

---
+++ BayesianAnalyses/assassination-hope.R
@@ -0,0 +1,16 @@
+required_packages = c("brms") # include list of packages needed here, e.g. required_packages = c("stats", "tidyverse", "lme4")
+
+# this for-loop checks each package is installed and then installs them if not
+for (package in required_packages) {
+  if(package %in% rownames(installed.packages()) == FALSE) {install.packages(package, repos = "http://cran.us.r-project.org", dependencies=TRUE, INSTALL_opts = "--no-lock")}
+}
+
+# include library(REQUIRED_PACKAGE) statement for each required package here
+library(brms)
+
+dataframe <- read.csv("assas.csv") # load your data from csv
+
+model <- brm(hope ~ year_number + after + year_after + loggdp_z + conflict_z + 
+                   (year_number + after + year_after|event_id), dataframe)  # run your model
+saveRDS(model, "model-hope.rds")       # save the model object for later reference
+summary(model)                    # output the summary of a model to the output file specified in your ".sh" file
